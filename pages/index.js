14b9f089d198a417ab8b976799ece187e571856a
Siri Tanguturi <siri.tanguturi1@gmail.com>
2022-11-30 17:20:24 -0500

    display all posts, TODO: pagination

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,28 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [posts, setPosts] = useState([]);
+  const [postDivs, setPostDivs] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/api/getAllPosts")
+      .then((response) => response.json())
+      .then((data) => {
+        setPosts(data);
+      });
+  }, []);
+
   return (
     <div className={styles.container}>
+      {posts.map(post => {
+      return(<div>
+        <h1>{post.title}</h1>
+        <p>{post.body}</p>
+      </div>);
+      })}
     </div>
   )
 }

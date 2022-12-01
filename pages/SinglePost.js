c60180d2c78cfbd37acb5c84faa2c29a59760cb0
JakeWang22 <jakewang2678@gmail.com>
2022-12-01 07:00:58 -0500

    small changes

--- pages/SinglePost.js
+++ pages/SinglePost.js
@@ -1,44 +1,46 @@
 import React from "react";
 import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+
 
 
 function SinglePost() {
     const [edit, setEdit] = useState(false);
+    const [comments, setComments] = useState([]);
+    const [posts, setPosts] = useState([]);
+    const router = useRouter();
+
     const onEdit = () => {
         setEdit(!edit);
     }
+    const addComment = () => {
+        comments.unshift({})
+    }
 
-    const [comments, setComments] = useState([]);
-    
 
-    /*const refreshData = () => {
-        router.replace(router.asPath);
-      };
-    const deletePost = async () => {
-        try {
-          await fetch(`/pages/SinglePost/${postId}`, {
-            method: "DELETE",
-          });
-          if (redirect) {
-            router.push(redirect)
-          } else {
-            refreshData();
-          }
-        } catch (error) {
-          console.error(error);
-        }
-       };
-       */
+    useEffect(() => {
+        fetch("http://localhost:3000/api/getAllPosts")
+        .then((response) => response.json())
+        .then((data) => {
+            setPosts(data);
+        });
+    }, []);
+
     return (
         <div style={{backgroundColor:"#101010"}}>
-            <button onClick={deletePost}>&lt;</button>
-            <h style={{display:"flex", flexDirection:"column", alignItems:"center", fontSize:30}}>Title</h>
-            <p style={{fontSize:15}}>Date</p>
-            <p contentEditable={edit} style={{fontSize:22}}>Body</p>
+            <button onClick={onEdit}>&lt;</button>
+            <h style={{display:"flex", flexDirection:"column", alignItems:"center", fontSize:30}}>{posts.title}</h>
+            <p style={{fontSize:15}}>{posts.date}</p>
+            <p contentEditable={edit} style={{fontSize:22}}>{posts.body}</p>
             <button onClick={onEdit}>Edit Post</button>
-            <button onClick={deletePost}>Delete Post</button>
+            <button onClick={onEdit}>Delete Post</button>
             <p style={{backgroundColor:"#151515"}}>Comments</p>
-            <button onClick={deletePost}>Add Comment</button>
+            <button onClick={onEdit}>Add Comment</button>
+            {
+                comments.map(prev => {
+                    return <p>{posts.comments}</p>
+                })
+            }
         </div>
     )
 }
